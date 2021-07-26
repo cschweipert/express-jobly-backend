b0071bb582362b3af6b92b6a5b1b8a6409113850
Christina Schweipert <c.schweipert@gmail.com>
2021-07-26 16:18:45 -0600

    disable ssl

--- db.js
+++ db.js
@@ -5,10 +5,17 @@
 const { Client } = require("pg");
 const { getDatabaseUri } = require("./config");
 
-const db = new Client({
-  connectionString: getDatabaseUri(),
-});
+let config = { connectionString: getDatabaseUri() };
+if (process.env.NODE_ENV === 'production') {
+  config.ssl = { rejectUnauthorized: false }
+}
+const db = new Client(config)
+// const db = new Client({
+//   connectionString: getDatabaseUri(),
+// });
 
 db.connect();
 
 module.exports = db;
+
+
